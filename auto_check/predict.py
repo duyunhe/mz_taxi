742cb53792a56d3aa7e1c9e71e2c14be0a8e6486
duyunhe <a_crux@126.com>
2019-07-03 17:08:54 +0800

    bp nn

---
+++ auto_check/predict.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/3 15:57
+# @Author  : yhdu@tongwoo.cn
+# @简介    : 
+# @File    : predict.py
+
+
+from network import NeuralNetwork
+import numpy as np
+from math import log
+
+
+def read_data():
+    f_list = []
+    with open('../data/all_feature.csv') as fp:
+        for line in fp:
+            items = map(float, line.strip('\n').split(','))
+            items[3] = log(items[3] + 1)
+            f_list.append(items)
+    x = np.array(f_list)
+    x_norm = x / x.max(axis=0)
+    return x_norm
+
+
+def judge(x):
+    return 1 if x > .5 else 0
+
+
+def main():
+    X = read_data()
+    i_num, h_num, o_num, learning_rate = 5, 6, 1, 0.1
+    nn = NeuralNetwork(i_num, h_num, o_num, learning_rate)
+    data = X[:, 0:i_num]
+    o = X[:, i_num:].T
+    for i in range(1000):
+        row = X.shape[0]
+        for j in range(row):
+            input_list, target = X[j, :i_num], X[j, i_num:]
+            nn.train(input_list, target)
+        print i, nn.calc_loss(data, o)
+
+    q = [0, 0, 0.5, 0, 0]
+    print nn.query(q)
+
+
+main()
